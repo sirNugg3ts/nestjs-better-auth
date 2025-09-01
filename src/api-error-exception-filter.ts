8b63fae8c6d3dc5a80328772b18257bfaa1799f0
Thalles Passos <contato@thalles.me>
2025-08-31 22:20:38 -0300

    remove catch idk

--- src/api-error-exception-filter.ts
+++ src/api-error-exception-filter.ts
@@ -1,10 +1,8 @@
 import type { ArgumentsHost } from "@nestjs/common";
-import { Catch } from "@nestjs/common";
 import type { ExceptionFilter } from "@nestjs/common";
 import { APIError } from "better-auth/api";
 import type { Response } from "express";
 
-@Catch()
 export class APIErrorExceptionFilter implements ExceptionFilter {
 	catch(exception: APIError, host: ArgumentsHost): void {
 		console.log("im getting called");
