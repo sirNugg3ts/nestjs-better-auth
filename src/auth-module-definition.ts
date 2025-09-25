46014a19f6f3ec3f35b9039e34113682967aa439
Thalles Passos <contato@thalles.me>
2025-09-25 11:13:59 -0300

    remove exception filters

--- src/auth-module-definition.ts
+++ src/auth-module-definition.ts
@@ -1,10 +1,8 @@
 import { ConfigurableModuleBuilder } from "@nestjs/common";
 import type { Auth } from "./auth-module.ts";
-import { APIErrorHookThrow } from "./api-error-hook-throw.ts";
 
 export type AuthModuleOptions<A = Auth> = {
 	auth: A;
-	enableAPIErrorThrow?: boolean;
 	disableTrustedOriginsCors?: boolean;
 	disableBodyParser?: boolean;
 };
@@ -19,16 +17,11 @@ export const { ConfigurableModuleClass, OPTIONS_TYPE, ASYNC_OPTIONS_TYPE } =
 		.setExtras(
 			{
 				isGlobal: true,
-				enableAPIErrorThrow: false,
 				disableTrustedOriginsCors: false,
 				disableBodyParser: false,
 			},
 			(def, extras) => {
 				const providers = def.providers ?? [];
-
-				if (extras.enableAPIErrorThrow) {
-					providers.push(APIErrorHookThrow);
-				}
 
 				return {
 					...def,
